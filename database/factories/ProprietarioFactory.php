61f8e5e9a00ab4d36e37a071cdc4ed6d55a736c5
xBruno1000x <fariabruno10@gmail.com>
2025-10-17 14:32:05 -0300

    :recycle: refactor: Realiza melhorias nos forms de taxas

--- database/factories/ProprietarioFactory.php
+++ database/factories/ProprietarioFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Proprietario;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class ProprietarioFactory extends Factory
 {
@@ -12,14 +13,13 @@ class ProprietarioFactory extends Factory
 
     public function definition(): array
     {
-        $uniq = substr((string) time() . random_int(100, 999), -10);
+        $uniqueSuffix = strtolower((string) Str::ulid());
         return [
-            'nome' => 'Proprietario Teste ' . $uniq,
-            'cpf' => str_pad((string) random_int(10000000000, 99999999999), 11, '0', STR_PAD_LEFT),
-            // Ensure telefone fits validation (max 15 chars)
-            'telefone' => '+5511' . random_int(900000000, 999999999),
-            'email' => 'owner+' . $uniq . '@example.com',
-            'password' => password_hash('password', PASSWORD_BCRYPT),
+            'nome' => 'Proprietario Teste ' . $uniqueSuffix,
+            'cpf' => $this->faker->unique()->numerify('###########'),
+            'telefone' => '+55' . $this->faker->numerify('11#########'),
+            'email' => 'owner+' . $uniqueSuffix . '@example.com',
+            'password' => Hash::make('password'),
         ];
     }
 }
